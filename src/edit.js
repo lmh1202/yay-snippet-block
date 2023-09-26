4ac4f19a43da7b62e72244395165ee9680bb1fb3
lmh1202 <lmhao1202@gmail.com>
2023-09-26 09:59:13 +0700

    First commit

--- src/edit.js
+++ src/edit.js
@@ -2,15 +2,17 @@ import { __ } from '@wordpress/i18n';
 
 import { useBlockProps } from '@wordpress/block-editor';
 
-import './editor.scss';
+import ServerSideRender from '@wordpress/server-side-render';
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const blockProps = useBlockProps();
+	// console.log(attributes)
 	return (
-		<p {...useBlockProps()}>
-			{__(
-				'Yay Snippet Block – hello from the editor!',
-				'yay-snippet-block'
-			)}
-		</p>
+		<p {...blockProps}>
+			<ServerSideRender
+				block='create-block/yay-snippet-block'
+				attributes={attributes}
+			/>
+		</ p>
 	);
 }
--- yay-snippet-block.php
+++ yay-snippet-block.php
@@ -5,15 +5,36 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            ONYX-yaycommerce
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       yay-snippet-block
  *
  * @package create-block
  */
-function create_block_yay_snippet_block_block_init()
-{
-    register_block_type(__DIR__ . '/build');
+
+if(!defined('ABSPATH')) {
+    die('We\' are sorry, but you can not directly access this file.');
 }
-add_action('init', 'create_block_yay_snippet_block_block_init');
+
+define('YAYSB_VERSION', '1.0.0');
+define('YAYSB_PLUGIN_FILE', __FILE__);
+define('YAYSB_PLUGIN_PATH', plugin_dir_path(YAYSB_PLUGIN_FILE));
+define('YAYSB_PLUGIN_URL', plugin_dir_url(YAYSB_PLUGIN_FILE));
+define('YAYSB_IS_DEVELOPMENT', true);
+
+add_action(
+    'init',
+    function () {
+        register_block_type(
+            YAYSB_PLUGIN_PATH.'/build', [
+            'render_callback' => 'render_yay_snippet_block',
+            ] 
+        );
+    }
+);
+
+function render_yay_snippet_block($block_attributes,$content)
+{
+    return sprintf("<p class=''>%s</p>", esc_attr('This is new block', 'yay-snippet-block'));
+}
